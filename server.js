d0c662dad4409df1a5e3f4bb574d1cbd172b0e7c
Lisa Brown <lisaann493@verizon.net>
2022-07-18 20:28:26 -0400

    changed cors origin for deployment

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const app = express()
 
 // config & middleware
 require('dotenv').config()
-app.use(cors({ origin: 'http://localhost:3000' }))
+const corsOptions = {
+    origin: '*'
+}
+// app.use(cors({ origin: 'http://localhost:3000' }))
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+const path = require('path')
 
 
 // home
@@ -28,6 +33,18 @@ app.use('/workout-schedule', wkoSchedulesController)
 const workoutsList = require('./controllers/workoutsList')
 app.use('/workout-list', workoutsList)
 
+// serve static front end in production mode
+if (process.env.NODE_ENV === "development") {
+    app.use(express.static(path.join(__dirname, 'client', 'build')));
+  }
+
+  // Root
+app.get('*', (req, res) => {
+    res.status(404).json({
+      message: 'Not Found',
+    })
+  })
+  
 // start server
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`)
