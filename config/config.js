3ae3c5e7a7ab2129625a443e4ea547b83fd1dcfb
Lisa Brown <lisaann493@verizon.net>
2022-07-22 11:57:18 -0400

    change prod configs

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+// const Pool = require('pg').Pool
 
 module.exports = {
   "development": {
@@ -19,7 +20,22 @@ module.exports = {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
-    "host": "127.0.0.1",
+    "host": process.env.PG_HOST,
     "dialect": "postgres"
   }
 }
+
+// const isProduction = process.env.NODE_ENV === 'production'
+
+// const connectionString = 
+// `postgresql: //${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.PG_HOST}
+// :${process.env.PG_PORT}/${process.env.DB_DATABASE}`
+
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL :
+//   connectionString,
+//     ssl: {
+//       rejectUnauthorized: false
+//     }
+// })
+// module.exports = pool
